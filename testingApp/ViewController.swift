f34d9bdc9402b0219fc6c623f850c8c782cd1a3f
abdelhadi29 <abh_bm@hotmail.com>
2025-01-05 11:14:32 +0100

    Initial Commit

---
+++ testingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testingApp
+//
+//  Created by abdel on 5/1/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
